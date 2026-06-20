d0a451da57fb74c760e4ff131e292d3f5677a836
MikesDevCorner <wagnmich@gmail.com>
2026-06-20 13:40:01 +0200

    [bump version to 26.0]

--- core/src/com/xsheetgames/Configuration.java
+++ core/src/com/xsheetgames/Configuration.java
@@ -19,7 +19,7 @@ public class Configuration {
 
 
 	/************INTERN VERSION INFO*****************************/
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "26.0";
 	public static final String VERSION_DATE = "20.06.2026";
 	/************************************************************/
 
